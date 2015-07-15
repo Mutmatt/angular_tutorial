54dd79c7371245a3b122d58aa8f52e7a37f9844e
Matt Erickson <Matt@MattErickson.me>
2015-07-15 10:50:02 -0500

    Init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTutApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularTutApp
+ */
+angular.module('angularTutApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTutApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularTutApp
+ */
+angular.module('angularTutApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
